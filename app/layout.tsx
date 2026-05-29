f64291e1a0d8208c7054a71262fa49e9447ad00a
wanwisa sukkhala <59186548+wanwisasukkhala@users.noreply.github.com>
2026-05-29 14:54:08 +0700

    update vercle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Prompt } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "@/components/ClientLayout"; // import ตัวที่เราสร้างตะกี้
+import { Analytics } from '@vercel/analytics/react';
 
 const prompt = Prompt({
   subsets: ["thai", "latin"],
@@ -11,9 +12,16 @@ const prompt = Prompt({
 
 export const metadata: Metadata = {
   title: "Wanwisa.skl",
-  description: "พอร์ตโฟลิโอ Full-stack Developer โดย วันวิสา สุขคะละ",
+  description: "Profile wanwisa.skl",
   icons: {
-    icon: "/Image/icon/icons8-code-94.png",
+    icon: [
+      { url: "/Image/icon/icons8-code-94.png" }, // ไอคอนหลักสำหรับ Browser ทั่วไป
+      { url: "/Image/icon/icons8-code-94.png", sizes: "94x94", type: "image/png" } // ระบุขนาดและประเภทให้ชัดเจน
+    ],
+    // เผื่อไว้สำหรับเวลาคนแชร์ลิงก์หรือเซฟหน้าเว็บลงบน iPhone/iPad (Apple Touch Icon)
+    apple: [
+      { url: "/Image/icon/icons8-code-94.png", sizes: "94x94", type: "image/png" }
+    ]
   },
 };
 
@@ -29,6 +37,7 @@ export default function RootLayout({
         <ClientLayout>
           {children}
         </ClientLayout>
+        <Analytics />
       </body>
     </html>
   );
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* คีย์หลักอื่นๆ ของคุณ (ถ้ามี) เช่น output, images ฯลฯ */
+  
+  // ย้ายมาไว้ตรงนี้เลย ไม่ต้องใส่ใน experimental แล้วครับ
+  allowedDevOrigins: ['192.168.110.2', 'localhost:3000'], 
 };
 
 export default nextConfig;
